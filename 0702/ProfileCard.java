9949d847c48db7bada1d3b2a6f07b0a61cfda31d
Jason119801 <jasonsam980@gmail.com>
2026-07-02 10:37:27 +0800

    實作題

---
+++ 0702/ProfileCard.java
@@ -0,0 +1,11 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        String name = "Amy";
+        String department = "Information Management";
+        String course = "Programming";
+        System.out.println("=== Profile ===");
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Favorite Course: " + course);
+    }
+}
