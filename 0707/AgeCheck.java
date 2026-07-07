93f687998886b1841336cf93dc8084101d959880
Jason119801 <jasonsam980@gmail.com>
2026-07-07 10:16:17 +0800

    123

---
+++ 0707/AgeCheck.java
@@ -0,0 +1,8 @@
+public class AgeCheck {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean pass = age >= 18;
+        System.out.println("Age: " + age);
+        System.out.println("Adult: " + pass);
+    }
+}
